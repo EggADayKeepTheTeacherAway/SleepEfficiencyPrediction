adb2c3ea6700bb277dc28bb5cb2aba2ef83aca7f
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2025-04-18 02:36:59 +0700

    refactor: changed flask to fastapi

---
+++ backend/schemas.py
@@ -0,0 +1,53 @@
+from pydantic import BaseModel, Field
+
+
+# Pydantic models
+class Latest(BaseModel):
+    user_id: int
+    sleep_id: int
+    ts: str
+    temperature: float
+    humidity: float
+    heartrate: int
+
+class Efficiency(BaseModel):
+    light: float
+    rem: float
+    deep: float
+    smoke: bool
+    exercise: int
+    efficiency: float
+
+class LogItem(BaseModel):
+    user_id: int
+    sleep_id: int
+    ts: str
+    temperature: float
+    humidity: float
+    heartrate: int
+
+class SessionInfo(BaseModel):
+    sleep_id: int
+    start_time: str
+    end_time: str
+
+class UserRegister(BaseModel):
+    username: str
+    password: str
+    age: int
+    gender: str
+    smoke: bool
+    exercise: int
+
+class UserEdit(BaseModel):
+    user_id: int
+    username: str
+    password: str
+    age: int
+    gender: str
+    smoke: bool
+    exercise: int
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
